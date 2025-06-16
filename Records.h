5dbc1de333f917a03ce4906d6daed4100ec9c4e3
BabySnapz <babysnaposu@gmail.com>
2025-06-16 21:26:34 +0800

    Updated records header and implementation

--- Records.h
+++ Records.h
@@ -5,19 +5,20 @@
 
 class Records
 {
-	friend std::ostream& operator<<(std::ostream& out, const Records& aRecord);
+	friend std::ostream& operator<<(std::ostream &out, const Records &aRecord);
 	private:
 		std::string id;
 		std::string location;
 		std::string time;
 
 	public:
+		Records(std::string id_, std::string location_, std::string time_);
 		const std::string& getID();
-		void setID(std::string id);
+		void setID(const std::string &id_);
 		const std::string& getLocation();
-		void setLocation(std::string location);
+		void setLocation(const std::string &location_);
 		const std::string& getTime();
-		void setTime(std::string location);
+		void setTime(const std::string &time_);
 		void showDetails() const;
 };
 
