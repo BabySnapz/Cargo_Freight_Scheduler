e6b93e7c609c776990ce6caef06b6c27c9eeeed8
jkk0h <jiakaikoh@hotmail.com>
2025-07-23 22:36:16 +0800

    edited sortalgo, schedulermanager

--- Header Files/SortAlgorithms.h
+++ Header Files/SortAlgorithms.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <unordered_map>
 #include <vector>
+#include <string>
 #include <utility>
+#include <algorithm>
+
 #include "Freight.h"
 #include "Cargo.h"
+
 
 class SortAlgorithms
 {
